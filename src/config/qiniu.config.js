b1c762ca516f2b5bbb94316741e70903aa3372eb
ATQQ <2604395430@qq.com>
2020-02-19 20:45:41 +0800

    :rocket:更新图床的token

--- src/config/qiniu.config.js
+++ src/config/qiniu.config.js
@@ -3,7 +3,7 @@ let config = {
     useCdnDomain: true,
     region: qiniu.region.z0
 }
-let token = "17iBaqCDcWUjh-6fUN4KWy3hm7HydYm5Fc1ruakj:7O9PF05PvT6EVdFbVtQoDOMEt9M=:eyJzY29wZSI6Im1kc291cmNlIiwiZGVhZGxpbmUiOjE1ODE5NDMxNzl9"
+let token = "17iBaqCDcWUjh-6fUN4KWy3hm7HydYm5Fc1ruakj:iPaREitB2YmTYjXb--bAYgcBrVE=:eyJzY29wZSI6Im1kc291cmNlIiwiZGVhZGxpbmUiOjE1ODQ3MDc3Mjd9"
 let Domain = 'http://img.cdn.sugarat.top'
 export {
     config,
