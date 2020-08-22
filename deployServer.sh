429607cc8b9d60de5f7ad223ca1022e949c0bbdd
ATQQ <2604395430@qq.com>
2020-08-22 11:08:43 +0800

    feat: 增加部署脚本

---
+++ deployServer.sh
@@ -0,0 +1,13 @@
+compressDir="./build"               # 压缩目录 
+compressFile="result.tar.gz"        # 压缩后的文件名
+user="root"                         # 远程登录用户
+origin="sugarat.top"                # 远程登录origin
+targetDir="/www/wwwroot/imgbed"     # 目标目录
+echo "开始-----归档解压"
+tar -zvcf ${compressFile} ${compressDir}
+echo "开始-----拷贝至服务器"
+scp ${compressFile} ${user}@${origin}:./
+echo "开始-----部署"
+ssh -p22 ${user}@${origin} "tar -xf ${compressFile} && rm -rf ${targetDir}/build/* && mv build/* ${targetDir}/build && rm -rf ${compressFile}"
+echo "部署完成"
+rm -rf ${compressFile} 
--- src/config/qiniu.config.js
+++ src/config/qiniu.config.js
@@ -3,7 +3,7 @@ let config = {
     useCdnDomain: true,
     region: qiniu.region.z0
 }
-let token = "17iBaqCDcWUjh-6fUN4KWy3hm7HydYm5Fc1ruakj:iPaREitB2YmTYjXb--bAYgcBrVE=:eyJzY29wZSI6Im1kc291cmNlIiwiZGVhZGxpbmUiOjE1ODQ3MDc3Mjd9"
+let token = "17iBaqCDcWUjh-6fUN4KWy3hm7HydYm5Fc1ruakj:oNpiijb8eKV71jDP_tdoxwcaTfo=:eyJzY29wZSI6Im1kc291cmNlIiwiZGVhZGxpbmUiOjE2MDA2NTc0MzB9"
 let Domain = 'http://img.cdn.sugarat.top'
 export {
     config,
